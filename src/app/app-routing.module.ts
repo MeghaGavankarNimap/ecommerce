0600b6c479b44afe6538e45b2adf81303eed02cc
meghagavankar1 <meghagavankar@nimapinfotech.com>
2023-12-26 17:19:01 +0530

    added modules ,navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=>import('../app/modules/view/show/show.module').then(m=>m.ShowModule)
+ 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
